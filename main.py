4147a1de41cd16c51a94c199d9e1a70c0bc1d5af
Mekhrona <mehrona.nm@gmail.com>
2025-03-08 05:42:14 +0500

    added get balance method for wallet

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from configs.config import settings
 from db.models import migrate_tables
 from pkg.controllers.default import router as default_router
 from pkg.controllers.auth import router as auth_router
+from pkg.controllers.wallet import router as wallet_router
 
 app = FastAPI() 
 
@@ -17,5 +18,6 @@ if __name__ == "__main__":
     # Подключаем маршруты
     app.include_router(default_router)
     app.include_router(auth_router)
+    app.include_router(wallet_router)
 
     uvicorn.run(app, port=settings.port, host=settings.host)
--- pkg/services/wallet.py
+++ pkg/services/wallet.py
@@ -1,6 +1,18 @@
-
 from logger.logger import logger
 from pkg.repositories import wallet as wallet_repository
 from schemas import wallet as wallet_schema
 from db.models import Wallet
 
+def get_wallet_balance(user_id: int):
+    try:
+        # Fetch the wallet by user ID
+        wallet = wallet_repository.get_wallet_by_user_id(user_id)
+        if wallet:
+            logger.info(f"Wallet balance retrieved for user {user_id}.")
+            return float(wallet.balance)  # Convert Decimal to float
+        else:
+            logger.warning(f"No wallet found for user {user_id}.")
+            return            
+    except Exception as e:
+        logger.error(f"Error retrieving wallet balance for user {user_id}: {e}")
+        
